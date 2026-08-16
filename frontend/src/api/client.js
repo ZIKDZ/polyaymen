98f5d3feb50df5813dfc93a7c912d1cf2b965b72
ILIAS <iliase.zaoui@gmail.com>
2026-08-16 18:35:19 +0100

    Default API_BASE to relative /api instead of hardcoded localhost

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE = import.meta.env.VITE_API_BASE || "http://127.0.0.1:8000/api";
+export const API_BASE = import.meta.env.VITE_API_BASE || "/api";
 
 export const api = axios.create({ baseURL: API_BASE });
 
